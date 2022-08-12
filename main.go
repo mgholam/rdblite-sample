7a8857616a8459cc69008c7da8dbc1382191ddaf
M. Gholam <mgholam@gmail.com>
2022-08-12 21:29:18 +0430

    - updated to new rdblite

--- main.go
+++ main.go
@@ -158,6 +158,7 @@ func rdbtest() {
 
 	// db.Table1.Delete(99999)
 	log.Println("id 99,999 =", db.Table1.FindByID(99_999))
+	log.Println("id invalid =", db.Table1.FindByID(-1))
 	fmt.Println()
 
 	str = "Fort"
